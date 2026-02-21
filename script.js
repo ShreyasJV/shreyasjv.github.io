e53ef48b81aa454f7602498df8ff4e7ca84b7433
S JEKKI VENKATESHWARULU <58063371+ShreyasJV@users.noreply.github.com>
2026-02-21 14:13:23 +0000

    Implement theme toggle functionality

---
+++ script.js
@@ -0,0 +1,15 @@
+const toggle = document.getElementById("theme-toggle");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("light");
+
+  if (document.body.classList.contains("light")) {
+    localStorage.setItem("theme", "light");
+  } else {
+    localStorage.setItem("theme", "dark");
+  }
+});
+
+if (localStorage.getItem("theme") === "light") {
+  document.body.classList.add("light");
+}
